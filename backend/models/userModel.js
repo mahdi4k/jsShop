d56b39e4a8fcaecb014c94668a0caf7a3bca2f29
mahdi2k <falahati.mahdi@yahoo.com>
2021-01-07 14:15:55 +0330

    data inserted to mongodb

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -26,3 +26,5 @@ const userSchema = mongoose.Schema({
 },{
     timestamps : true
 })
+const User = mongoose.model('User',userSchema)
+export default User
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 import express from 'express'
 import connectDB from './config/db.js'
 import products from './data/products.js'
-import { config } from 'dotenv'
+import  dotenv  from 'dotenv'
 const app = express()
 
-config()
+dotenv.config()
 
 connectDB()
 
@@ -13,8 +13,8 @@ app.get('/api/products',(req,res)=>{
 })
 
 app.get('/api/products/:id',(req,res)=>{
-    const product = find( product => product._id === req.params.id)
-    res.json(product)
+    const product = products.find( product => product._id === req.params.id)
+    res.json(product) 
 })
 
 app.listen(5000 , console.log(`Server running on port ${process.env.PORT}`)) 
