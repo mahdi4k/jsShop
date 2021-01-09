3ed52685069bcaa74b98e5a264c79bc5438d4d5e
mahdi2k <falahati.mahdi@yahoo.com>
2021-01-09 15:19:11 +0330

    custom middleware

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,28 +1,29 @@
 import express from 'express'
 import asyncHandler from 'express-async-handler'
-import Product from '../models/productModel'
+import Product from '../models/productModel.js'
+
 const router = express.Router()
 
 
-// Feth all Ptoducts
+// Fetch all Products
 // GET /api/products
 // desc Fetch all products
 
-router.get('/', asyncHandler (async(req,res)=>{
-    const product = await Product.find({})
+router.get('/', asyncHandler(async (req, res) => {
+    const products = await Product.find({})
+    res.json(products)
 }))
 
-
-// Feth single Ptoducts
+// Fetch single Products
 // GET /api/products
-router.get('/:id',asyncHandler((req,res)=>{
-     const product = await Product.findById(req.params.id)
+router.get('/:id', asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
 
-     if(product){
-         res.json(product)
-     } else {
-         res.status(404).json({ message : 'Product not found' })
-     }
+    if (product) {
+        res.json(product)
+    } else {
+        res.status(404).json({message: 'Product not found'})
+    }
 }))
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,29 @@ dotenv.config()
 
 connectDB()
 
+
+
+
+app.get('/',(req,res)=>{
+    res.send('api is running')
+
+})
+
 app.use('/api/products',productRoutes)
 
-app.listen(5000 , console.log(`Server running on port ${process.env.PORT}`)) 
+app.use((req,res,next) =>{
+    const error = new Error('not found')
+    res.status(404)
+    next(error)
+})
+
+app.use((err,req,res,next) =>{
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+    res.status(statusCode)
+    res.json({
+        message : err.message,
+        stack : process.env.NODE_ENV === 'production' ? null : err.stack
+    })
+})
+
+app.listen(5000, console.log(`Server running on port ${process.env.PORT}`))
