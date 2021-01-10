ed756c5f3a1ceacc74af2f4f132fc4cbb1d56449
mahdi2k <falahati.mahdi@yahoo.com>
2021-01-10 15:07:32 +0330

    start including redux

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import  express from 'express'
 import  connectDB from './config/db.js'
 import  dotenv  from 'dotenv'
+import {notFound , errorHandler} from "./middleware/errorMiddleware.js";
 import  productRoutes from './routes/productRoutes.js'
 
 const app = express()
@@ -19,19 +20,8 @@ app.get('/',(req,res)=>{
 
 app.use('/api/products',productRoutes)
 
-app.use((req,res,next) =>{
-    const error = new Error('not found')
-    res.status(404)
-    next(error)
-})
+app.use(notFound)
 
-app.use((err,req,res,next) =>{
-    const statusCode = res.statusCode === 200 ? 500 : res.statusCode
-    res.status(statusCode)
-    res.json({
-        message : err.message,
-        stack : process.env.NODE_ENV === 'production' ? null : err.stack
-    })
-})
+app.use(errorHandler)
 
 app.listen(5000, console.log(`Server running on port ${process.env.PORT}`))
